615870e67c6abb861f93f40a01b72b1d2592c56c
Alfie Darko <me@alfiedarko.co.uk>
2018-06-28 12:15:12 +0100

    GREEN: CoinChanger > .returnChange() > returns 2x2p and 1p for 5p" "

--- src/CoinChanger.js
+++ src/CoinChanger.js
@@ -1,12 +1,45 @@
 function CoinChanger() {
-  this.coins = {1 : '1p'}
+  this.coins = {
+    2: '2p',
+    1 : '1p',
+  }
 }
 
 CoinChanger.prototype.returnChange = function(amount) {
   var change = []
-  if (this.coins.hasOwnProperty(amount)) {
-    change.push(this.coins[amount])
-  }
+
+  var amountLeft = amount
+
+  Object.keys(this.coins).reverse().forEach((coin) => {
+    // console.log(coin, 'key');
+    // console.log(this.coins[coin], 'this.coins[key]]');
+
+    // console.log(Object.values(key), 'coinindex');
+    // console.log(Object.keys(key));
+    // console.log(array, 'array');
+    //   console.log(Object.getOwnPropertyNames(parseInt(array[key])), 'lool')
+
+
+
+    while (amountLeft > 0) {
+      console.log(amountLeft, 'before while');
+      if(amountLeft >= coin){
+        console.log(amountLeft, 'amount left at the beginnning of loop');
+        // while (coin > amountLeft) {
+        amountLeft = amountLeft - coin
+        console.log(amountLeft, ' amount left after taking coin away');
+
+        change.push(this.coins[coin])
+        console.log(change, 'after pushing amount to change');
+      } else {
+        break;
+      }
+
+
+    }
+  })
+
+
   return change
 }
 
--- test/coinChanger.test.js
+++ test/coinChanger.test.js
@@ -3,9 +3,12 @@ var expect = require('chai').expect;
 var CoinChanger = require('../src/CoinChanger')
 
 describe('CoinChanger', function() {
-  console.log( CoinChanger)
   var coinChanger = new CoinChanger()
   it('returns 1p for 1', function() {
     expect(coinChanger.returnChange(1)).to.eql(['1p'])
+  });
+
+  it('returns 2x2p and 1p for 5p', function() {
+    expect(coinChanger.returnChange(5)).to.eql(['2p', '2p', '1p']);
   });
 });
